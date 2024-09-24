557239b4875467f4b3a1d2d30f2d1e727710e47f
Piotr Słupski <piotr.slupski17@gmail.com>
2024-09-24 16:38:53 +0200

    added separators to my approach section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import AboutMe from "./components/about-me/AboutMe";
+import MyApproach from "./components/my-approach/MyApproach";
 
 function App() {
   return (
@@ -7,9 +8,13 @@ function App() {
         title="Webdev"
         subtitle="With an abundant tech stack experience"
         description="Hi, my name is Peter. I'm currently studying IT in Wrocław. Since high school I've been passionate about web development. I enjoy learning new techniques and acquiring programming skills. I also put big effort on self-improvement. Constructive criticism is important for me, because I can learn a lot from my mistakes.
+        I'm quite social person despite being programmer. Spending time with different people and speaking to them is crucial part of my life. I'm interested in football, basketball and esports. Aditionally I love reading books no matter what type they are.
+        Hi, my name is Peter. I'm currently studying IT in Wrocław. Since high school I've been passionate about web development. I enjoy learning new techniques and acquiring programming skills. I also put big effort on self-improvement. Constructive criticism is important for me, because I can learn a lot from my mistakes.
         I'm quite social person despite being programmer. Spending time with different people and speaking to them is crucial part of my life. I'm interested in football, basketball and esports. Aditionally I love reading books no matter what type they are."
         location="Wrocław, Poland"
       />
+
+      <MyApproach />
     </div>
   );
 }
--- src/components/my-approach/MyApproach.tsx
+++ src/components/my-approach/MyApproach.tsx
@@ -1,7 +1,18 @@
 import styles from "./my-approach.module.css";
 
 const MyApproach = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className={styles["separator-top"]}></div>
+      <div className={styles.separator}></div>
+      <section className={styles.section}>
+        <h3 className="header-title">My Approach</h3>
+
+        <div className={styles.cards}></div>
+      </section>
+      <div className={styles["separator-bottom"]}></div>
+    </>
+  );
 };
 
 export default MyApproach;
